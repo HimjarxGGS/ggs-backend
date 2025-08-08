0f69333327983689ae727a0b4e125650a9f5e81f
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-08-09 06:59:04 +0700

    fixing search on photo gallery

--- resources/views/filament/resources/pendaftar-event-resource/actions/search-inline.blade.php
+++ resources/views/filament/resources/pendaftar-event-resource/actions/search-inline.blade.php
@@ -1,20 +1,14 @@
 <div class="flex items-center gap-3">
     <form class="flex items-center gap-2" onsubmit="return false;">
-        <input
-            type="search"
-            id="filament-search-inline"
-            placeholder="Cari nama atau nama file..."
-            class="filament-toolbar-input rounded px-3 py-2 w-80"
-            aria-label="Search registrants"
-            oninput="Livewire.dispatch('searchUpdated', [this.value])"
-        />
+        <x-filament::input.wrapper suffix-icon="heroicon-o-magnifying-glass">
+            <x-filament::input
+                type="search"
+                id="filament-search-inline"
+                placeholder="Cari foto"
+                class="w-full"
+                aria-label="Search registrants"
+                oninput="Livewire.dispatch('searchUpdated', [this.value])" />
+        </x-filament::input.wrapper>
 
-        <button
-            type="button"
-            onclick="Livewire.dispatch('searchUpdated', '')"
-            class="inline-flex items-center gap-2 px-3 py-2 rounded border"
-        >
-            Clear
-        </button>
     </form>
 </div>
