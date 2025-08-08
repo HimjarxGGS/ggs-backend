af1e0019df88078f1ee19c30962b1a1eb080e949
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-08-08 11:20:55 +0700

    GRIIDDDD

--- resources/views/filament/resources/pendaftar-event-resource/pages/photo-gallery.blade.php
+++ resources/views/filament/resources/pendaftar-event-resource/pages/photo-gallery.blade.php
@@ -1,44 +1,30 @@
 <x-filament::page>
-    <x-slot name="header">
-        <div class="flex items-center justify-between w-full">
-            <h2 class="text-2xl font-bold">Foto Pendaftar: {{ $this->event->name }}</h2>
-            <div class="flex space-x-2">
-                {{-- Search bar --}}
-                <x-filament::input
-                    placeholder="Cari Foto…"
-                    wire:model.debounce.500ms="search"
-                    icon="heroicon-o-search"
-                />
-
-                {{-- Download all button --}}
-                <x-filament::button
-                    wire:click="downloadAll"
-                    icon="heroicon-o-arrow-down-tray"
-                    color="success"
-                >
-                    Download Semua
-                </x-filament::button>
-            </div>
-        </div>
-    </x-slot>
 
     {{-- Gallery grid --}}
-    <div class="grid grid-cols-4 gap-6 mt-4">
+    <x-filament::grid
+        style="--cols-lg: repeat(3, minmax(0, 1fr));"
+        class="lg:grid-cols-[--cols-lg]">
         @forelse ($this->getRegistrants() as $pendaftar)
-            <div class="bg-white rounded-lg shadow overflow-hidden">
-                <img
-                    src="{{ Storage::url($pendaftar->registrant_picture) }}"
-                    class="w-full h-48 object-cover"
-                    alt="{{ $pendaftar->nama_lengkap }}"
-                >
-                <div class="p-2 text-sm font-medium">
+        <div class="max-w-sm bg-orange-50 rounded-lg shadow-lg  justify-center grid">
+            <img class="w-full h-48 object-cover"
+                src="{{ $pendaftar->registrant_picture}}"
+                alt="Event Image">
+            <span class="flex p-2 w-full">
+                <div class="p-2 text-sm font-medium w-full">
                     {{ $pendaftar->nama_lengkap }}
                 </div>
-            </div>
+
+                <div class="p-2 text-sm font-medium">
+                    <a href="">Download</a>
+                </div>
+
+            </span>
+        </div>
+
         @empty
-            <div class="col-span-4 text-center text-gray-500 py-10">
-                Tidak ada foto yang cocok dengan "{{ $this->search }}"
-            </div>
+        <div class="col-span-4 text-center text-gray-500 py-10">
+            Tidak ada foto yang cocok dengan "{{ $this->search }}"
+        </div>
         @endforelse
-    </div>
+    </x-filament::grid>
 </x-filament::page>
