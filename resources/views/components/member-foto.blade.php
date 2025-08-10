305df3a40f3dfa9334b7ba70cc4c6de907b1c46b
Farid | Iamjustzero <imjustzero@duck.com>
2025-08-10 12:26:57 +0700

    image to left

--- resources/views/components/member-foto.blade.php
+++ resources/views/components/member-foto.blade.php
@@ -4,14 +4,14 @@
     $url = asset('storage/' . ltrim($path, '/'));
 @endphp
 
-{{-- <div style="text-align:center; padding:.5rem 0;">
+<div style="text-align:center; padding:.5rem 0;">
     <img src="{{ $url }}" alt="Foto Member" style="max-width:200px; border-radius:8px;">
-</div> --}}
+</div>
 
-<div class="flex justify-center">
+{{-- <div class="flex justify-center">
     <img src="{{ $getRecord()->pendaftar?->registrant_picture 
         ? asset('storage/' . $getRecord()->pendaftar->registrant_picture) 
         : asset('storage/images/dummy.png') }}"
         alt="Foto Member"
         style="max-width:200px; border-radius:8px;">
-</div>
+</div> --}}
