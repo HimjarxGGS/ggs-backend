b4df03cfef4f45e030649a0022a18fbee21ebc70
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-08-14 08:44:54 +0700

    merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
