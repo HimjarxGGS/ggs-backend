e0463a9667b18d5cf597824710d965f3122a747a
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-10-06 09:31:19 +0700

    testing download registrant picture

--- app/Http/Controllers/EventPhotoController.php
+++ app/Http/Controllers/EventPhotoController.php
@@ -148,6 +148,7 @@ class EventPhotoController extends Controller
             return response()->download(url($path), $filename);
         }
         
+        dd(url($path), $filename);
         abort(400, "File doesn't exists or not found");
     }
 
